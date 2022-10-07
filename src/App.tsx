094c7f2072d89245d14524dbb024a90d536e1529
Kirill Inoz <63226427+kirillinoz@users.noreply.github.com>
2022-10-07 08:31:18 +0200

    Update 1.1.4 :wrench: - Splitbee analytics added - Google analytics removed

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,13 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import ReactGA from 'react-ga'
 
 import Home from './views/Home'
 import Editor from './views/Editor'
 import Header from './components/Header'
 import Footer from './components/Footer'
 
-// Google Analytics
-const MEASUREMENT_ID = 'G-2SC4HXDQNM'
-ReactGA.initialize(MEASUREMENT_ID)
-
 function App() {
     const [ready, setReady] = useState<boolean>(false)
-
-    useEffect(() => {
-        ReactGA.pageview(window.location.pathname + window.location.search)
-    }, [])
 
     return (
         <BrowserRouter>
