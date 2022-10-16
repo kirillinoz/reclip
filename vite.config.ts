4e6276c44ec0bc7b2f3eb1aedb709c32583c5d06
Kirill Inoz <63226427+kirillinoz@users.noreply.github.com>
2022-10-16 13:18:02 +0200

    Update 1.1.11 :wrench: - Fixing analytics

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import crossOriginIsolation from 'vite-plugin-cross-origin-isolation'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(), crossOriginIsolation()]
+    plugins: [react()]
 })
